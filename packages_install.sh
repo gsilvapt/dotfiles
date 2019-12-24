d6ec0e78cf1d769b806677e241cf25dfa0c7c2a0
Gustavo Silva <gustavosantaremsilva@gmail.com>
2019-12-24 11:25:50 +0000

    Fixes #4

--- packages_install.sh
+++ packages_install.sh
@@ -31,7 +31,7 @@ echo "[SCIRPT] Prepare Nano for all environments"
 find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> ~/.nanorc
 
 echo "[SCRIPT] Installing Vim Vundle and custom Plugins"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vimcurl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
