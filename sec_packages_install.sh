f29be7cbf3d99ad67a4d76cdb2ebd176e11c84c9
Gustavo Silva <gustavosantaremsilva@gmail.com>
2020-05-29 16:08:39 +0100

    Add automated security scripts

--- sec_packages_install.sh
+++ sec_packages_install.sh
@@ -1,45 +1,97 @@
 #!/bin/bash
 
 ## Essential packages
-echo "[SCRIPT] Update and Install APT packages"
+echo "update and install apt packages"
 sudo apt update && sudo apt upgrade -y
-sudo apt install git vim tmux build-essential cmake python3-dev
+sudo apt install -y git vim tmux build-essential cmake python3-dev libcurl4-openssl-dev libssl-dev jq ruby-full libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev build-essential libssl-dev libffi-dev python-dev python-setuptools libldns-dev python3-pip python-pip python-dnspython git rename xargs
 
-echo "[SCRIPT] Install Gogh Themes for Ubuntu Terminal"
-read -p "Install SPACEGRAY || Press enter to continue"
-bash -c  "$(wget -qO- https://git.io/vQgMr)"
 
-## Utils needed for pentest
-sudo apt install dirbuster gobuster exploitdb nmap
+#install go
+echo "installing golang"
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile
+source ~/.bash_profile
+sleep 1
+break
 
-## FZF
-echo "[SCRIPT] Installing FZF"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#create a tools folder in ~/
+mkdir ~/tools
+cd ~/tools/
 
-echo "[SCRIPT] Moving config files before installing things"
-cp .tmux.conf ~/.tmux.conf
+echo "installing Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r*
+pip install -r requirements.txt
+cd ~/tools/
+echo "done"
+
+
+echo "installing wpscan"
+git clone https://github.com/wpscanteam/wpscan.git
+cd wpscan*
+sudo gem install bundler && bundle install --without test
+cd ~/tools/
+echo "done"
+
+echo "installing sqlmap"
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+cd ~/tools/
+echo "done"
+
+echo "installing knock.py"
+git clone https://github.com/guelfoweb/knock.git
+cd ~/tools/
+echo "done"
+
+echo "installing nmap"
+sudo apt-get install -y nmap
+echo "done"
+
+echo "installing massdns"
+git clone https://github.com/blechschmidt/massdns.git
+cd ~/tools/massdns
+make
+cd ~/tools/
+echo "done"
+
+echo "installing asnlookup"
+git clone https://github.com/yassineaboukir/asnlookup.git
+cd ~/tools/asnlookup
+pip install -r requirements.txt
+cd ~/tools/
+echo "done"
+
+echo "installing httprobe"
+go get -u github.com/tomnomnom/httprobe
+echo "done"
+
+echo "downloading Seclists"
+cd ~/tools/
+git clone https://github.com/danielmiessler/SecLists.git
+cd ~/tools/SecLists/Discovery/DNS/
+##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
+cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
+cd ~/tools/
+echo "done"
+
+echo "move configurations around"
 cp .vimrc ~/.vimrc
-cp .zshrc ~/.zshrc
-cp .gitconfig ~/.gitconfig
-cp .conkyrc ~/.conkyrc
+cp .tmux.conf ~/.tmux.conf
 
-echo "[SCIRPT] Prepare Nano for all environments"
-find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> ~/.nanorc
-
-echo "[SCRIPT] Installing Vim Vundle and custom Plugins"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vimcurl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+echo "install vim plugins"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts && ./install.sh && cd .. && rm -rf fonts/
 
-echo "[SCRIPT] Installing ZSH and desired plugins."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+ls -la
+echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
 
-echo "[SCRIPT] Starting Tmux and running plugin installation"
-read -p "After entering tmux, press prefix + I to install everything. Now press enter to continue"
-tmux source ~/.tmux.conf 
